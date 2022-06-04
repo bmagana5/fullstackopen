50be0bbbcf9ce76834808e5cdf7459f03d36f135
Brian Magana <bmagana5@yahoo.com>
2022-06-04 16:07:02 -0700

    notes app for part 2a

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,73 +1,26 @@
-import { useState } from "react";
+import Note from './components/Note';
 
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  );
-};
-
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  );
-};
-
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    );
-  }
-  return (
-    <div>
-      button press history: {allClicks.join(' ')}
-    </div>
-  );
-};
-
-// complex state example
-// const App = () => {
-//   const [ clicks, setClicks ] = useState({ left: 0, right: 0 });
-
-//   const handleLeftClicks = () => setClicks({...clicks, left: clicks.left + 1});
-//   const handleRightClicks = () => setClicks({...clicks, right: clicks.right + 1});
-
+// const Display = ({counter}) => {
 //   return (
-//     <div>
-//       <Display counter={clicks.left} />
-//       <Button onClick={handleLeftClicks} text={"left"}/>
-//       <Button onClick={handleRightClicks} text={"right"}/>
-//       <Display counter={clicks.right} />
-//     </div>
+//     <div>{counter}</div>
 //   );
 // };
 
-const App = () => {
-  const [ clicks, setClicks ] = useState({ left: 0, right: 0 });
-  const [ allClicks, setAll ] = useState([]);
+// const Button = ({ onClick, text }) => {
+//   return (
+//     <button onClick={onClick}>{text}</button>
+//   );
+// };
 
-  const handleLeftClicks = () => {
-    setAll(allClicks.concat('L'));
-    setClicks({ ...clicks, left: clicks.left + 1 });
-  }
-
-  const handleRightClicks = () => {
-    setAll(allClicks.concat('R'));
-    setClicks({ ...clicks, right: clicks.right + 1 });
-  }
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Display counter={clicks.left}/>
-      <Display counter={clicks.right}/>
-      <Button onClick={handleLeftClicks} text={"left"}/>
-      <Button onClick={handleRightClicks} text={"right"}/>
-      <History allClicks={allClicks} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => <Note key={note.id} note={note} />)}
+      </ul>
     </div>
   );
-
 };
 
 export default App;
