1ad9e1de71f2be36ac940ff50ea44c020463e6fb
Brian Magana <bmagana5@yahoo.com>
2022-06-04 01:35:25 -0700

    completed part1d 6 through 11

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,58 +1,98 @@
-const Header = (props) => {
+import { useState } from 'react';
+
+const Header = ({ text }) => <h1>{text}</h1>;
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+
+const StatisticLine = ({ text, value }) => <tr><td>{text}</td><td>{value}</td></tr>;
+
+const Total = ({ ratings }) => {
+  let sum = 0;
+  Object.keys(ratings).forEach(key => sum += ratings[key]);
   return (
-    <h1>{props.course}</h1>
+    <tr>
+      <td>all</td><td>{sum}</td>
+    </tr>
+  );
+};
+
+const Average = ({ ratings }) => {
+  let sum = 0;
+  Object.keys(ratings).forEach(key => sum += ratings[key]);
+  let average = (ratings.good - ratings.bad) / sum;
+  if (isNaN(average)) {
+    return (
+      <tr>
+        <td>average</td><td>0</td>
+      </tr>
+    );
+  }
+  return (
+    <tr>
+      <td>average</td><td>{average}</td>
+    </tr>
   )
 };
 
-const Part = (props) => {
+const Positive = ({ ratings }) => {
+  let sum = 0;
+  Object.keys(ratings).forEach(key => sum += ratings[key])
+  let positive = ratings.good / sum * 100;
+  if (isNaN(positive)) {
+    return (
+      <tr>
+        <td>positive</td><td>0 %</td>
+      </tr>
+    );
+  }
   return (
-    <p>
-      {props.p["name"]} {props.p["exercises"]}
-    </p>
-  );
-}
-
-const Content = (props) => {
-  return (
-    <>
-      {props.parts.map(p => <Part p={p} />)}
-    </>
+    <tr>
+      <td>positive</td><td>{positive} %</td>
+    </tr>
   );
 };
 
-const Total = (props) => {
+const Statistics = ({ ratings }) => {
   let sum = 0;
-  props.parts.forEach(p => sum += p.exercises)
+  Object.keys(ratings).forEach(key => sum += ratings[key]);
+  if (sum === 0) {
+    return (
+      <div> 
+        No feedback given
+      </div>
+    );
+  }
   return (
-    <p>Number of exercise {sum}</p>
+    <table>
+      <tbody>
+        {Object.keys(ratings).map(key => <StatisticLine text={key} value={ratings[key]} key={key}/>)}
+        <Total ratings={ratings} />
+        <Average ratings={ratings} />
+        <Positive ratings={ratings} />
+      </tbody>
+    </table>
   );
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
+  const [ratings, setRatings] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
 
+  const handleGoodButton = () => setRatings({ ...ratings, good: ratings.good + 1});
+  const handleNeutralButton = () => setRatings({ ...ratings, neutral: ratings.neutral + 1});
+  const handleBadButton = () => setRatings({ ...ratings, bad: ratings.bad + 1});
 
   return (
     <div>
-      <Header course={course["name"]} />
-      <Content parts={course["parts"]}/>
-      <Total parts={course["parts"]}/>
+      <Header text={"give feedback"} />
+      <Button onClick={handleGoodButton} text={"good"} />
+      <Button onClick={handleNeutralButton} text={"neutral"} />
+      <Button onClick={handleBadButton} text={"bad"} />
+      <Header text={"statistics"} />
+      <Statistics ratings={ratings} />
     </div>
   );
 };
