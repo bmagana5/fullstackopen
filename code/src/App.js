bca5390db363a5901f62f2a4d33e59dfcd5ebdbd
Brian Magana <bmagana5@yahoo.com>
2022-09-19 13:13:44 -0700

    changed folder name for simplicity

---
+++ code/src/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.p} {props.e}
+    </p>
+  );
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part p={props.p1} e={props.e1} />
+      <Part p={props.p2} e={props.e2} />
+      <Part p={props.p3} e={props.e3} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>Number of exercise {props.e1 + props.e3 + props.e3}</p>
+  );
+};
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content p1={part1} p2={part2} p3={part3} 
+              e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+    </div>
+  );
+};
+
+export default App;
