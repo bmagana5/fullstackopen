524ea91ef99eb202c142702631940e5cc863a162
Brian Magana <bmagana5@yahoo.com>
2022-10-09 02:18:43 -0700

    completed part 2a of the full stack open course, exercises 2.1-2.5

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# fullstackopen" 
+
+This branch contains the code for part 2 of the Full Stack Open course found at:
+https://fullstackopen.com/en/part2/rendering_a_collection_modules
--- code/src/App.js
+++ code/src/App.js
@@ -1,49 +1,55 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.p} {props.e}
-    </p>
-  );
-}
-
-const Content = (props) => {
-  return (
-    <>
-      <Part p={props.p1} e={props.e1} />
-      <Part p={props.p2} e={props.e2} />
-      <Part p={props.p3} e={props.e3} />
-    </>
-  );
-};
-
-const Total = (props) => {
-  return (
-    <p>Number of exercise {props.e1 + props.e3 + props.e3}</p>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
 
   return (
     <div>
-      <Header course={course} />
-      <Content p1={part1} p2={part2} p3={part3} 
-              e1={exercises1} e2={exercises2} e3={exercises3}/>
-      <Total e1={exercises1} e2={exercises2} e3={exercises3}/>
+      <h1>Web development curriculum</h1>
+      <div>{courses.map(course => <Course key={course.id} course={course}/>)}</div>
     </div>
   );
 };
