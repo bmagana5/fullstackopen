a99d71c2910ad48bcc44570689241e6da3f4bc35
Brian Magana <bmagana5@yahoo.com>
2022-11-05 16:31:27 -0700

    updated setupProxy path from '' to '/api'

--- code/src/setupProxy.js
+++ code/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
-    app.use('/', createProxyMiddleware({
+    app.use('/api', createProxyMiddleware({
         target: 'http://localhost:8080',
         changeOrigin: true
     }));
