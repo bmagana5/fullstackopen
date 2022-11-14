14acf1e7df832bebad4961e1d83d1147c4e4023e
Brian Magana <bmagana5@yahoo.com>
2022-11-14 12:40:01 -0800

    added validation to Mongo schemas

--- backend/index.js
+++ backend/index.js
@@ -25,53 +25,18 @@ const requestLogger = (request, response, next) => {
 
 server.use(requestLogger);
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      date: "2022-05-30T17:30:31.098Z",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2022-05-30T18:39:34.091Z",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2022-05-30T19:20:14.298Z",
-      important: true
-    }
-];
-
-
-const generateId = () => {
-    let maxId = Math.floor(Math.random() * 1000);
-    while (notes.find(n => n.id === maxId)) {
-        maxId = Math.floor(Math.random() * 1000);
-    }
-    return maxId;
-};
-
-server.post('/api/notes', (request, response) => {
+server.post('/api/notes', (request, response, next) => {
     const body = request.body;
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        });
-    }
-
     const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date()
     });
-    note.save().then(savedNote => {
-        response.json(savedNote);
-    });
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote);
+        })
+        .catch(error => next(error));
 });
 
 server.get('/', (request, response) => {
@@ -85,7 +50,6 @@ server.get('/api/notes', (request, response) => {
 });
 
 server.get('/api/notes/:id', (request, response, next) => {
-        
     Note.findById(request.params.id)
         .then(note => {
             if (note) {
@@ -98,14 +62,10 @@ server.get('/api/notes/:id', (request, response, next) => {
 });
 
 server.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body;
+    const { content, important } = request.body;
 
-    const newNote = {
-        content: body.content,
-        important: body.important
-    }
     // use {new:true} to have updated note be returned by the Promise in then() as the updatedNote parameter in callback
-    Note.findByIdAndUpdate(request.params.id, newNote, { new: true })
+    Note.findByIdAndUpdate(request.params.id, { content, important }, { new: true, runValidators: true, context: 'query' })
         .then(updatedNote => {
             response.json(updatedNote);
         })
@@ -132,9 +92,10 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message);
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' });
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message });
     }
-
-    // if not a CastError, send to Express' default error handling middleware
+    // if not a CastError or ValidationError, send to Express' default error handling middleware
     next(error);
 };
 
--- backend/models/note.js
+++ backend/models/note.js
@@ -3,8 +3,15 @@ const mongoose = require('mongoose');
 const url = process.env.MONGODB_NOTES_APP_URI;
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
     important: Boolean
 });
 
